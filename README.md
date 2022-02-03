42c7e51426474ea683fc203786f56df68ee87e24
emylincon <emylincon@gmail.com>
2022-02-03 22:07:43 +0000

    refactored for create

--- README.md
+++ README.md
@@ -3,3 +3,23 @@ pkg create-python-app or pkg create-go-app
 
 ## Contributing
 * application is designed using [cobra](https://www.linode.com/docs/guides/using-cobra/)
+
+### Create new commands
+* command
+```
+cobra add <command_name>
+```
+* example
+```
+cobra add create
+```
+
+### create subcommands
+* subcommand
+```
+cobra add <subcommand_name> -p 'mainCommand_name'
+```
+* example
+```
+cobra add deactivate -p 'pythonCmd'
+```
---
+++ cmd/createPython.go
@@ -0,0 +1,64 @@
+/*
+Copyright © 2022 Emeka Ugwuanyi <emylincon@gmail.com>
+
+*/
+
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// createPythonCmd represents the create command
+var createPythonCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Creates a basic python app template dev",
+	Long:  `Creates a basic python app template dev`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Println("create called")
+		dir, err := cmd.Flags().GetString("directory")
+		if err != nil {
+			return err
+		}
+		name, err := cmd.Flags().GetString("name")
+		if err != nil {
+			return err
+		}
+		email, err := cmd.Flags().GetString("email")
+		if err != nil {
+			return err
+		}
+
+		python(dir, name, email)
+		return nil
+	},
+}
+
+func init() {
+	pythonCmd.AddCommand(createPythonCmd)
+	createPythonCmd.Flags().StringP("directory", "d", "", "directory for go app")
+	createPythonCmd.Flags().StringP("name", "n", "name", "name of developer")
+	createPythonCmd.Flags().StringP("email", "e", "email.gmail.com", "email address of developer")
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// createCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func python(directory, name, email string) error {
+	log.Println("directory: ", directory)
+	if err := os.Mkdir(directory, os.ModePerm); err != nil {
+		return err
+	}
+	return createEnvironment(directory, name, email, "python")
+}
