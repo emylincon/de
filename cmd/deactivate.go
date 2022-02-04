c6718172988b899b8e58163c5129ae6f6570deff
Emeka E Ugwuanyi <emylincon@gmail.com>
2022-02-04 18:45:17 +0000

    create venv (#14)

--- cmd/deactivate.go
+++ cmd/deactivate.go
@@ -6,8 +6,6 @@ Copyright © 2022 Emeka Ugwuanyi <emylincon@gmail.com>
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
@@ -21,8 +19,8 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("deactivate called")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return pythonVenvMgr.Deactivate()
 	},
 }
 
